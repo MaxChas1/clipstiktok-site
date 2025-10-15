3c4a0a95bc0d061a548b5b79ad59b3307a12bb04
MaxChas1 <chassaingmaxime53@gmail.com>
2025-10-15 20:45:19 +0200

    Update [id].js

--- pages/api/jobs/[id].js
+++ pages/api/jobs/[id].js
@@ -1,12 +1,20 @@
 // pages/api/jobs/[id].js
 const { getJob } = require('../../../lib/store');
 
-module.exports = function handler(req, res) {
+module.exports = async function handler(req, res) {
+  const { id } = req.query || {};
+
   if (req.method !== 'GET') {
+    res.setHeader('Allow', 'GET');
     return res.status(405).json({ error: 'Méthode non autorisée' });
   }
-  const { id } = req.query || {};
-  const job = getJob(id);
-  if (!job) return res.status(404).json({ error: 'Job introuvable' });
-  return res.status(200).json({ job });
+
+  try {
+    const job = getJob(id);
+    if (!job) return res.status(404).json({ error: 'job introuvable' });
+    return res.status(200).json(job);
+  } catch (err) {
+    console.error('/api/jobs/[id] error:', err);
+    return res.status(500).json({ error: 'Erreur serveur.' });
+  }
 };
