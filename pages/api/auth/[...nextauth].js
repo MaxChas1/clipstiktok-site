cc99c5dc62c289111fa4721129baa127df771065
MaxChas1 <chassaingmaxime53@gmail.com>
2025-10-23 23:22:47 +0200

    Configure NextAuth with Google provider only

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import AppleProvider from "next-auth/providers/apple";
-import EmailProvider from "next-auth/providers/email";
 
 export default NextAuth({
   providers: [
@@ -9,26 +7,9 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    AppleProvider({
-      clientId: process.env.APPLE_ID,
-      clientSecret: process.env.APPLE_SECRET,
-    }),
-    EmailProvider({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
-    }),
   ],
   session: {
     strategy: "jwt",
   },
-  callbacks: {
-    async session({ session }) {
-      if (session?.user?.email === process.env.NEXT_PUBLIC_CREATOR_EMAIL) {
-        session.user.isCreator = true;
-      } else {
-        session.user.isCreator = false;
-      }
-      return session;
-    },
-  },
+  secret: process.env.NEXTAUTH_SECRET,
 });
